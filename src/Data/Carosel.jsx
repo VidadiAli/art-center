35a09a6c055c7fb8651564b4601e78d7a309d8ab
VidadiAli <vidadiali.tiva@gmail.com>
2024-12-22 13:10:51 +0400

    first commit

--- src/Data/Carosel.jsx
+++ src/Data/Carosel.jsx
@@ -1,31 +1,46 @@
 export const carosel = [
     {
-        id: "1",
-        nameCarosel: 'Əl İşləri',
-        imageCarosel: 'https://github.com/web5bpm/artcenter-carousel-image/blob/main/el-ish.jpg',
-        textCarosel: `Azərbaycan'da əl işi sənəti, ölkənin zəngin mədəni irsi və tarixi ənənələrindən biri olaraq,
-əsrlər boyu inkişaf etmiş və bu gün də önəmli bir sənət sahəsi kimi qorunmuşdur.
-Azərbaycanlı sənətkarlar müxtəlif texnika və materiallardan istifadə edərək unikal əl işləri yaradırlar.
-Bu əl işləri, həm tarixi, həm də estetik baxımdan çox dəyərli olmaqla yanaşı, iqtisadi baxımdan da böyük əhəmiyyətə malikdir.
-`
+        id: "3",
+        nameCarosel: 'İnklüziv Sənətkarlıq',
+        imageCarosel: 'https://github.com/web5bpm/artcenter-carousel-image/blob/main/20241215_205817_0000.jpg',
+        textCarosel: `İnklüziv sənətkarlıq mövzusu çox vacib bir sosial və mədəni konsepsiyadır. 
+        İncəsənətin və sənətkarlığın inklüziv yanaşması, cəmiyyətin hər bir üzvünün, xüsusilə də xüsusi ehtiyacı 
+        olan insanların yaradıcılıq fəaliyyətlərinə qoşulmasını, bərabər imkanlara malik olmalarını təmin edir. 
+        Bu yanaşma, həm sosial ədalətin, həm də müxtəlif mədəniyyətlərin qorunmasının təməlini təşkil edir. 
+        Sənətkarlıq və inklüziv yanaşma, əl işləri, zərgərlik, toxuculuq, xalçaçılıq, rəssamlıq və 
+        digər sənət sahələrində fəaliyyət göstərən fərqli bacarıqlara malik şəxslərin cəmiyyətə 
+        daxil edilməsini və yaradıcılıqla məşğul olmalarını təşviq edir.`
     },
     {
         id: "2",
-        nameCarosel: 'Xalça',
-        imageCarosel: 'https://github.com/web5bpm/artcenter-carousel-image/blob/main/xalca.jpg',
-        textCarosel: `Azərbaycan xalçası, dünyaca məşhur və unikal mədəniyyət nümunələrindən biridir.
-Bu xalçalar, əsrlərdir Azərbaycan sənətkarlarının zəngin mədəni irsini, adət-ənənələrini və dünyagörüşlərini özündə əks etdirir.
-Azərbaycan xalçası UNESCO-nun Mədəni İrs siyahısına daxil edilib və bu, onun dünya miqyasında tanınmasına və qorunmasına kömək edir.
+        nameCarosel: 'Satış',
+        imageCarosel: 'https://github.com/web5bpm/artcenter-carousel-image/blob/main/20241215_202046_0000.jpg',
+        textCarosel: `Bu bölümün məqsədi, Azərbaycan sənətkarlığından çıxan fəqrli-fərqli əl işlərini geniş miqyasda 
+        dünyanın müxtəlif bucaqlarına çatdırmaqdır. Bu əl işləri əsrlərdir Azərbaycan sənətkarlarının 
+        zəngin mədəni irsini, adət-ənənələrini və dünyagörüşlərini özündə əks etdirir. 
+        Biz burada xalça toxuma, zərgərlik, əl işləri və s. kimi sahələrdən istifadə edib, bu məhsulları
+        müxtəlif istiqamətlərə  daşıyacağıq.
 `
     },
     {
-        id: "3",
-        nameCarosel: 'Zərgərlik',
-        imageCarosel: 'https://github.com/web5bpm/artcenter-carousel-image/blob/main/zerger.jpg',
-        textCarosel: `Azərbaycan zərgərlik sənəti, zəngin mədəniyyətimizin və sənət ənənələrimizin təcəssümüdür.
-Bu sənət əsrlərdən bəri Azərbaycanın müxtəlif bölgələrində inkişaf etmişdir və qədim tarixə,
-adət-ənənələrə və innovativ dizaynlara əsaslanır. Azərbaycan zərgərlik sənəti,
-bəzən dünya miqyasında yüksək dəyərə malik olan əsrarəngiz ornamentlər, əl işi texnikaları və unikallığı ilə seçilir.
+        id: "1",
+        nameCarosel: 'Təhsil',
+        imageCarosel: 'https://github.com/web5bpm/artcenter-carousel-image/blob/main/20241215_213613_0000.jpg',
+        textCarosel: `Artcenter Azerbaijan təhsil proqramları ilə sənətkarların bilik və bacarıqlarını 
+        artırmağı hədəfləyir. Təhsil bölümü üç əsas istiqaməti əhatə edir: qısa müddətli kurslar, master 
+        klasslar və inklüziv kurslar. Qısa müddətli kurslar fərdi bacarıqları inkişaf etdirmək və yeni 
+        texnikalar öyrənmək istəyənlər üçün nəzərdə tutulub. Master klasslar isə tanınmış sənətkarların 
+        iştirakı ilə peşəkar təcrübə mübadiləsini təmin edir..
 `
+    },
+    {
+        id: "4",
+        nameCarosel: 'Azərbaycan Sənətkarlığı',
+        imageCarosel: 'https://github.com/web5bpm/artcenter-carousel-image/blob/main/20241215_202838_0000.jpg',
+        textCarosel: `Azərbaycan zəngin mədəni irsi və sənətkarlıq ənənələri ilə tanınan qədim bir diyardır. 
+        Coğrafi mövqeyi, tarixi və mədəni müxtəlifliyi ölkənin sənətkarlıq sahəsində özünəməxsus izlər qoymasına 
+        səbəb olmuşdur. Əsrlər boyu formalaşmış sənətkarlıq sahələri yalnız gündəlik tələbatı ödəməklə 
+        kifayətlənməyib, həm də estetik zövqün göstəricisinə çevrilmişdir. Bu sənət sahələri nəinki ölkə daxilində, 
+        həm də beynəlxalq səviyyədə Azərbaycan mədəniyyətini tanıtmaqda mühüm rol oynamışdır.`
     }
 ]
