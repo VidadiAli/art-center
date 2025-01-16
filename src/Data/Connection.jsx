e6945f4c6132d03f03f427d83157d703ff473925
VidadiAli <vidadiali.tiva@gmail.com>
2025-01-16 12:59:48 +0400

    first commit

--- src/Data/Connection.jsx
+++ src/Data/Connection.jsx
@@ -2,12 +2,12 @@ export const connection = [
     {
         phone1Main: '+994703100911',
         phone1Show: '070-310-09-11',
-        gmail: ''
+        gmail: 'artcenterazerbaijan@gmail.com'
     },
     {
-        instagram: '',
+        instagram: 'https://www.instagram.com/artcenteraz',
         tiktok: '',
-        facebook: '',
-        youtube: ''
+        facebook: 'https://www.facebook.com/profile.php?id=61571711383865',
+        youtube: 'https://www.youtube.com/@artcenterazerbaijan'
     }
 ]
